b66a73974981911af28d4f63d5c36646c22b20dc
Ahmad Salah <ahmad.salah1293@gmail.com>
2023-05-17 20:04:49 +0300

    quiz web app

---
+++ QuezApp/Controllers/ParticipantController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuezApp.Models;
+
+namespace QuizApp.Controllers
+{
+    public class ParticipantController : Controller
+    {
+        private readonly QuizDbContext _context;
+
+        public ParticipantController(QuizDbContext context)
+        {
+            _context = context;
+        }
+
+       
+
+        [HttpPost]
+        public async Task<ActionResult<Participant>> PostParticipant(Participant participant)
+        {
+            var temp = _context.Participants
+                .Where(x => x.Name == participant.Name
+                && x.Email == participant.Email)
+                .FirstOrDefault();
+
+            if (temp == null)
+            {
+                _context.Participants.Add(participant);
+                await _context.SaveChangesAsync();
+            }
+            else
+                participant = temp;
+
+            return Ok(participant);
+        }
+
+       
+    }
+}
---
+++ QuezApp/Controllers/QuestionController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuezApp.Models;
+
+namespace QuizApp.Controllers
+{
+    public class QuestionController : Controller
+    {
+        private readonly QuizDbContext _context;
+
+        public QuestionController(QuizDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Question>>> GetQuestions()
+        {
+            var random5Qns = await (_context.Questions
+                 .Select(x => new
+                 {
+                     QnId = x.QnId,
+                     QnInWords = x.QnInWords,
+                     ImageName = x.ImageName,
+                     Options = new string[] { x.Option1, x.Option2, x.Option3, x.Option4 }
+                 })
+                 .OrderBy(y => Guid.NewGuid())
+                 .Take(5)
+                 ).ToListAsync();
+
+            return Ok(random5Qns);
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Question>> GetQuestion(int id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return question;
+        }
+
+     
+        [HttpPost]
+        [Route("GetAnswers")]
+        public async Task<ActionResult<Question>> RetrieveAnswers(int[] qnIds)
+        {
+            var answers = await (_context.Questions
+                .Where(x => qnIds.Contains(x.QnId))
+                .Select(y => new
+                {
+                    QnId = y.QnId,
+                    QnInWords = y.QnInWords,
+                    ImageName = y.ImageName,
+                    Options = new string[] { y.Option1, y.Option2, y.Option3, y.Option4 },
+                    Answer = y.Answer
+                })).ToListAsync();
+            return Ok(answers);
+        }
+
+       
+
+    }
+}
